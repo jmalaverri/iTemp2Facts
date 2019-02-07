211c7f9c49d2fb4e668f043d331497e9bf118d31
jmalaverri <jgonzalesm1@gmail.com>
2019-02-07 17:10:43 +0100

    update tmethod clean data

--- utils.py
+++ utils.py
@@ -2,29 +2,43 @@ import re
 import datetime
 #import fileinput
 
+def fix(st):
+    t = st.find('-')
+    if t > 0:
+        rest = st[t:]
+        y = st[:t]
+    else:
+        rest = ''
+        y = st
+        
+    return '%04d%s'%(int(y), rest)
+
 # Parse a string date from Yago into a YYYY-MM-DD format.
 def parse_date(dt):
     m = re.search('\d{0,4}\-(\d|#){0,2}-(\d|#){0,2}', dt)
     try:
         if m is None:
             #date = "0"
-            return "0"
+            return ''
 
         date = m.group(0)
         if date.find("#") > -1:
             date = date.replace("#", "").strip('-')
 
-        return date
+        return fix(date)
     except ValueError:
-        pass
+        return ''
 
 
 def strtodate(string):
+    if string is None:
+        return None
     for fmt in ["%Y-%m-%d", "%Y-%m", "%Y"]:
         try:
             return datetime.datetime.strptime(string, fmt).date()
         except ValueError:
             continue
+    return None
 
 
 # def rename_predicate(file_name):
